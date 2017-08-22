0cb01742a89bc7945e952347a5711e5fd2e0a793
MChrisDE <mchris@outlook.de>
2017-08-22 20:13:40 +0200

    now work on every resolution but not resizable anymore

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.floatlayout import FloatLayout
 from kivy.properties import ObjectProperty, NumericProperty
 from kivy.clock import Clock
 from kivy.vector import Vector
@@ -11,9 +12,11 @@ os.environ['KIVY_GL_BACKEND'] = 'angle_sdl2'
 Config.set('graphics', 'width', '1920')
 Config.set('graphics', 'height', '1080')
 Config.set('graphics', 'fullscreen', 'auto')
+Config.set('graphics', 'resizable', 0)
+from kivy.core.window import Window
 
 
-class GameScreen(Widget):
+class GameScreen(FloatLayout):
     player = ObjectProperty(None)
     ud_rocket = ObjectProperty(None)
     lr_rocket = ObjectProperty(None)
@@ -31,7 +34,7 @@ class GameScreen(Widget):
 
 
 class Player(Widget):
-    wannabe = [625, 625]
+    wannabe = [Window.width / 2, Window.height / 2]
     vector = Vector(0, 0)
 
     def on_touch_move(self, touch):
@@ -51,34 +54,34 @@ class Player(Widget):
 
 
 class UPDOWNRocket(Widget):
-    vector = Vector(0, 6)
+    vector = Vector(0, Window.height * 0.0055)
     direction = 0
 
     def move(self):
-        if self.pos[1] > 1080 or self.pos[1] < -451:
-            if self.vector[1] < 15:
-                self.vector[1] = self.vector[1] * 1.2
+        if self.pos[1] > Window.height or self.pos[1] < - Window.height * 0.417:
+            if self.vector[1] < Window.height * 0.025:
+                self.vector[1] = self.vector[1] * 1.1
             self.direction = randint(0, 1) * 180
             if self.direction == 0:
-                self.pos = (randint(0, 1450), -450)
+                self.pos = (randint(0, int(Window.width * 0.7552)), -Window.height * 0.417)
             else:
-                self.pos = (randint(0, 1450), 999)
+                self.pos = (randint(0, int(Window.width * 0.7552)), Window.height * 0.925)
         self.pos = Vector(*self.vector).rotate(self.direction) + self.pos
 
 
 class LEFTRIGHTRocket(Widget):
-    vector = Vector(6, 0)
+    vector = Vector(Window.width * 0.003125, 0)
     direction = 0
 
     def move(self):
-        if self.pos[0] > 1500 or self.pos[0] < -451:
-            if self.vector[0] < 15:
+        if self.pos[0] > Window.width * 0.78125 or self.pos[0] < -Window.width * 0.2348:
+            if self.vector[0] < Window.width * 0.01875:
                 self.vector[0] = self.vector[0] * 1.1
             self.direction = randint(0, 1) * 180
             if self.direction == 0:
-                self.pos = (-450, randint(0, 950))
+                self.pos = (-Window.width * 0.2348, randint(0, int(Window.height * 0.9537)))
             else:
-                self.pos = (1500, randint(0, 950))
+                self.pos = (Window.width * 0.78125, randint(0, int(Window.height * 0.9537)))
         self.pos = Vector(*self.vector).rotate(self.direction) + self.pos
 
 
