aae9da2439bc4d5721814d79af20c0d1863138ca
MChrisDE <mchris@outlook.de>
2017-08-23 13:34:36 +0200

    add restart button

--- main.py
+++ main.py
@@ -16,25 +16,35 @@ Config.set('graphics', 'resizable', 0)
 from kivy.core.window import Window
 
 
-class GameScreen(FloatLayout):
+class Game(FloatLayout):
     player = ObjectProperty(None)
     ud_rocket = ObjectProperty(None)
     lr_rocket = ObjectProperty(None)
     sideboard = ObjectProperty(None)
+    hits = NumericProperty(0)
 
     def __init__(self, **kwargs):
-        super(GameScreen, self).__init__(**kwargs)
+        super(Game, self).__init__(**kwargs)
+        self.clock = Clock.schedule_interval(self.update, 1.0 / 60.0)
 
     def update(self, dt):
         self.player.move()
         self.ud_rocket.move()
         self.lr_rocket.move()
         if self.player.collide_widget(self.ud_rocket) or self.player.collide_widget(self.lr_rocket):
-            self.sideboard.hits += 1
+            self.hits += 1
+            self.clock.cancel()
+
+    def reset(self):
+        self.clock.cancel()
+        self.player.reset()
+        self.ud_rocket.reset()
+        self.lr_rocket.reset()
+        self.clock()
 
 
 class Player(Widget):
-    wannabe = [Window.width / 2, Window.height / 2]
+    wannabe = [Window.width*0.390625, Window.height / 2]
     vector = Vector(0, 0)
 
     def on_touch_move(self, touch):
@@ -52,6 +62,10 @@ class Player(Widget):
             self.vector = self.vector.normalize() * 10
         self.pos = Vector(self.vector) + self.pos
 
+    def reset(self):
+        self.wannabe = [Window.width*0.390625, Window.height / 2]
+        self.pos = (Window.width / 2, Window.height / 2)
+
 
 class UPDOWNRocket(Widget):
     vector = Vector(0, Window.height * 0.0055)
@@ -68,6 +82,11 @@ class UPDOWNRocket(Widget):
                 self.pos = (randint(0, int(Window.width * 0.7552)), Window.height * 0.925)
         self.pos = Vector(self.vector).rotate(self.direction) + self.pos
 
+    def reset(self):
+        self.vector = Vector(0, Window.height * 0.0055)
+        self.direction = 0
+        self.pos = (0, -self.size[1])
+
 
 class LEFTRIGHTRocket(Widget):
     vector = Vector(Window.width * 0.003125, 0)
@@ -82,19 +101,22 @@ class LEFTRIGHTRocket(Widget):
                 self.pos = (-Window.width * 0.2348, randint(0, int(Window.height * 0.9537)))
             else:
                 self.pos = (Window.width * 0.78125, randint(0, int(Window.height * 0.9537)))
-        self.pos = Vector(*self.vector).rotate(self.direction) + self.pos
+        self.pos = Vector(self.vector).rotate(self.direction) + self.pos
+
+    def reset(self):
+        self.vector = Vector(Window.width * 0.003125, 0)
+        self.direction = 0
+        self.pos = (-self.size[0], 0)
 
 
 class SideBoard(Widget):
-    hits = NumericProperty(0)
+    pass
 
 
 class DodgeThisApp(App):
     def build(self):
         self.load_kv('main.kv')
-        game = GameScreen()
-        Clock.schedule_interval(game.update, 1.0 / 60.0)
-        return game
+        return Game()
 
 
 if __name__ == '__main__':
